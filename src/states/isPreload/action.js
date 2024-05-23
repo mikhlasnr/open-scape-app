2e87752dd0237325a539c8163a00e170176a38c4
bblastdev <hi@bblast.io>
2024-05-23 17:19:52 +0700

    full feature

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,12 +1,10 @@
-/**
- * @TODO: Define all the actions (creator) for the isPreLoad state
- */
-import api from '../../utils/api'
-import { setAuthUserActionCreator } from '../authUser/action'
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
 
 const ActionType = {
   SET_IS_PRELOAD: 'SET_IS_PRELOAD',
-}
+};
 
 function setIsPreloadActionCreator(isPreload) {
   return {
@@ -14,23 +12,25 @@ function setIsPreloadActionCreator(isPreload) {
     payload: {
       isPreload,
     },
-  }
+  };
 }
 
 function asyncPreloadProcess() {
   return async dispatch => {
+    dispatch(showLoading());
     try {
       // preload process
-      const authUser = await api.getOwnProfile()
-      dispatch(setAuthUserActionCreator(authUser))
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
     } catch (error) {
       // fallback process
-      dispatch(setAuthUserActionCreator(null))
+      dispatch(setAuthUserActionCreator(null));
     } finally {
       // end preload process
-      dispatch(setIsPreloadActionCreator(false))
+      dispatch(setIsPreloadActionCreator(false));
     }
-  }
+    dispatch(hideLoading());
+  };
 }
 
-export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess }
+export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess };
