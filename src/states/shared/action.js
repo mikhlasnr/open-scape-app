e7e846ea05f4b945348500be655ea646033da9cb
bblastdev <hi@bblast.io>
2024-05-23 17:12:18 +0700

    init

---
+++ src/states/shared/action.js
@@ -0,0 +1,19 @@
+import api from '../../utils/api'
+import { receiveTalksActionCreator } from '../talks/action'
+import { receiveUsersActionCreator } from '../users/action'
+
+function asyncPopulateUsersAndTalks() {
+  return async dispatch => {
+    try {
+      const users = await api.getAllUsers()
+      const talks = await api.getAllTalks()
+
+      dispatch(receiveUsersActionCreator(users))
+      dispatch(receiveTalksActionCreator(talks))
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+}
+
+export { asyncPopulateUsersAndTalks }
