b948127b35dae6938bb9b3eed8daf313cbebf7c4
bblastdev <hi@bblast.io>
2024-05-26 12:46:21 +0700

    practice test reducer

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import LoadingBar from 'react-redux-loading-bar'
+import React from 'react';
+import LoadingBar from 'react-redux-loading-bar';
 
 function Loading() {
   return (
     <div className="loading">
       {/* @TODO: use react-redux-loading-bar to show loading bar */}
-      <LoadingBar />
+      <LoadingBar style={{ background: 'red', height: 5 }} />
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,19 +1,22 @@
-import api from '../../utils/api'
-import { receiveTalksActionCreator } from '../talks/action'
-import { receiveUsersActionCreator } from '../users/action'
+import api from '../../utils/api';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
 
 function asyncPopulateUsersAndTalks() {
   return async dispatch => {
+    dispatch(showLoading());
     try {
-      const users = await api.getAllUsers()
-      const talks = await api.getAllTalks()
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
 
-      dispatch(receiveUsersActionCreator(users))
-      dispatch(receiveTalksActionCreator(talks))
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveTalksActionCreator(talks));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+    dispatch(hideLoading());
+  };
 }
 
-export { asyncPopulateUsersAndTalks }
+export { asyncPopulateUsersAndTalks };
